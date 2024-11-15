462117c3812b2e29f8997d15abd47bf48f4a7a15
Renato Rojas <roj23813@uvg.edu.gt>
2024-11-15 12:51:41 -0600

    error simple arreglado

--- app/src/main/java/com/uvg/labfinalplats/repositories/network/CoinAPI.kt
+++ app/src/main/java/com/uvg/labfinalplats/repositories/network/CoinAPI.kt
@@ -3,7 +3,7 @@ package com.uvg.labfinalplats.repositories.network
 import com.uvg.labfinalplats.data.network.dto.EntryCoinListDTO
 import com.uvg.labfinalplats.data.network.dto.EntryCoinProfileDTO
 import com.uvg.labfinalplats.repositories.network.util.NetworkError
-import com.uvg.labfinalplats.repositories.network.util.Resultx
+import com.uvg.labfinalplats.repositories.network.util.Result
 
 interface CoinAPI{
 suspend fun getAllCoins(): Result<EntryCoinListDTO,NetworkError>
