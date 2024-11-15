2f86f8934a0084e82706a2c79fada798038d6ca9
Renato Rojas <roj23813@uvg.edu.gt>
2024-11-15 13:11:02 -0600

    Solo le falta room-VM para funcionar :)

--- app/src/main/java/com/uvg/labfinalplats/dependencies/KtorDependencies.kt
+++ app/src/main/java/com/uvg/labfinalplats/dependencies/KtorDependencies.kt
@@ -1,2 +1,17 @@
 package com.uvg.labfinalplats.dependencies
 
+import com.uvg.labfinalplats.data.network.HttpClientFactory
+import io.ktor.client.HttpClient
+
+
+object KtorDependencies {
+    private var httpClient: HttpClient? = null
+
+    private fun buildHttpClient(): HttpClient = HttpClientFactory.create()
+
+    fun provideHttpClient(): HttpClient {
+        return httpClient ?: synchronized(this) {
+            httpClient ?: buildHttpClient().also { httpClient = it }
+        }
+    }
+}
